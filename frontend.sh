f4df0d57dc67380f54312344f47c1ee14d7a8a62
vasukanna <vasu.kannaluri@gmail.com>
2024-08-31 19:47:33 +0530

    commiting config

--- frontend.sh
+++ frontend.sh
@@ -25,9 +25,9 @@ ROOT_CHECK(){
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 is $R FAILED $N" | tee -a $LOG_FILE
+        echo -e "$2 is ... $R FAILED $N" | tee -a $LOG_FILE
     else
-        echo -e " $2 is $G SUCCESS $N " | tee -a $LOG_FILE
+        echo -e "$2 is...$G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 
